d26524a46d5900981e99c422833bc081b6310d3b
Wu zeyan <isu11003091A@cloud.isu.edu.tw>
2023-12-27 20:07:31 +0800

    Add files via upload

---
+++ src/pages/HomePage/HomePage.lazy.tsx
@@ -0,0 +1,12 @@
+import type React from 'react';
+import { lazy, Suspense } from 'react';
+
+const LazyHomePage = lazy(async () => await import('./HomePage'));
+
+const HomePage = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode; }): JSX.Element => (
+  <Suspense fallback={null}>
+    <LazyHomePage {...props} />
+  </Suspense>
+);
+
+export default HomePage;
---
+++ src/pages/HomePage/HomePage.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import HomePage from './HomePage';
+
+describe('<HomePage />', () => {
+  test('it should mount', () => {
+    render(<HomePage />);
+    
+    const homePage = screen.getByTestId('HomePage');
+
+    expect(homePage).toBeInTheDocument();
+  });
+});
