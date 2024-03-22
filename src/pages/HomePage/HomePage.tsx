693ba5b29d54c3e387c647a596f6ae20c213af43
ffaa0100aa <ffaa0100aa@gmail.com>
2024-03-22 22:49:49 +0800

    feat: add authentication functionality to HomePage and LogInPage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,17 @@
 import { type FC } from 'react';
 import './HomePage.scss';
 import Dashboard from '../../layout/Dashboard/Dashboard';
+import { useAuth } from '../../service/auth-service';
+
 interface HomePageProps {}
 
-const HomePage: FC<HomePageProps> = () => (
+const HomePage: FC<HomePageProps> = () => {
+  const auth = useAuth();
+  return (
   <div className="HomePage" data-testid="HomePage">
-   
+   {auth.currentUser?.name ?? '未登入'}
     <Dashboard />
-  </div>
-);
+  </div>);
+};
 
 export default HomePage;
