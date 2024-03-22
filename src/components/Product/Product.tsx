2cbfd5a48775e3ea5e633b13566debc940d41bbb
Olive020 <a0977062020@gmail.com>
2024-03-22 22:53:51 +0800

    feat: modify product data api

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Product.scss';
 import { Grid, AppBar, Toolbar, IconButton, InputBase, Box, Link as MuiLink, Container } from '@mui/material';
@@ -7,11 +7,23 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import { generateProductData, ProductItem } from '../../data/Product';
+import { useApi } from '../../service/api-service';
+import * as api from '../../service/api-interface';
 interface ProductProps { }
 
 const Product: FC<ProductProps> = () => {
-  let data = generateProductData(30);
+  // let data = generateProductData(30);
+  const [data, setData] = useState<api.Product[]>([]);
+  const apiService = useApi();
+  useEffect(() => {
+    async function getData() {
+      const mockApi = new api.MockApi();
+      let data = await mockApi.listProduct();
+      setData(data);
+    }
+    getData();
+  }, []);
+  
   return (
     <Container >
       <Grid container spacing={3}>
@@ -26,29 +38,29 @@ const Product: FC<ProductProps> = () => {
 }
 
 
-const ShopItem: FC<{ item: ProductItem }> = (props) => {
+const ShopItem: FC<{ item: api.Product }> = (props) => {
   const navigate = useNavigate();
-  return(
-  <Card sx={{  margin: 1, display: 'flex', flexDirection: 'column' }} onClick={()=>navigate(`/productDetails/${props.item.id}`)}>
-    <CardActionArea>
-      <CardMedia
-        component="img"
-        image={props.item.coverUrl}
-        alt={props.item.name}
-      />
-      <CardContent>
-        <Typography gutterBottom  component="div" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-        {props.item.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        {props.item.description}
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-  </Card>
+  return (
+    <Card sx={{ margin: 1, display: 'flex', flexDirection: 'column' }} onClick={() => navigate(`/productDetails/${props.item.id}`)}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={props.item.coverUrl}
+          alt={props.item.name}
+        />
+        <CardContent>
+          <Typography gutterBottom component="div" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+            {props.item.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.item.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
 
-)
-  }
+  )
+}
 
 
 
